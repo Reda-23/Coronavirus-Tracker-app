0db167cff62818e164065efbbcecd72764455823
Reda-23 <readhajjaj@gmail.com>
2022-11-09 20:14:03 +0100

    Creating the project

---
+++ src/main/java/org/coronavisrustracker/CoronavisrusTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.coronavisrustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavisrusTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronavisrusTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/coronavisrustracker/CoronavisrusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coronavisrustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavisrusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
