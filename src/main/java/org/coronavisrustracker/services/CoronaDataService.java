347cd3c16c7a7bb0e1f7f0fb0802ef649b9c5a06
Reda-23 <readhajjaj@gmail.com>
2022-11-10 16:53:50 +0100

    Setting the request and the client

--- src/main/java/org/coronavisrustracker/services/CoronaDataService.java
+++ src/main/java/org/coronavisrustracker/services/CoronaDataService.java
@@ -1,9 +1,19 @@
 package org.coronavisrustracker.services;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class CoronaDataService {
 
-    public static String  CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private static String  CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
 
+    public void fetchData(){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(CORONA_DATA_URL))
+                .build();
+    }
 
 }
