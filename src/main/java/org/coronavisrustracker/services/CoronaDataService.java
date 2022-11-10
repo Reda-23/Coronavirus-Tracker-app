5b9a1195b44d20943f6ac011c89809ae5dbc4915
Reda-23 <readhajjaj@gmail.com>
2022-11-10 16:49:48 +0100

    Imorting the data source url

--- src/main/java/org/coronavisrustracker/services/CoronaDataService.java
+++ src/main/java/org/coronavisrustracker/services/CoronaDataService.java
@@ -1,4 +1,9 @@
 package org.coronavisrustracker.services;
 
 public class CoronaDataService {
+
+    public static String  CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+
+
 }
