9bf7951ea77ea45ab71c7718aab5fdfb8bf89307
Reda-23 <readhajjaj@gmail.com>
2022-11-10 16:57:25 +0100

    Setting the service class ,getting the response

--- src/main/java/org/coronavisrustracker/services/CoronaDataService.java
+++ src/main/java/org/coronavisrustracker/services/CoronaDataService.java
@@ -1,19 +1,27 @@
 package org.coronavisrustracker.services;
 
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+@Service
 public class CoronaDataService {
 
     private static String  CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
 
-    public void fetchData(){
+    @PostConstruct
+    public void fetchData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(CORONA_DATA_URL))
                 .build();
+        HttpResponse<String> httpResponse = client.send(request,HttpResponse.BodyHandlers.ofString());
     }
 
 }
